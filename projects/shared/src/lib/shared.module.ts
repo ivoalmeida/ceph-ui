587836817795c92164fd8421b4462c6cf4720cff
Ivo Almeida <ialmeida@redhat.com>
2025-05-27 13:19:03 +0100

    lib1

---
+++ projects/shared/src/lib/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { DataTableModule } from './datatable/datatable.module';
+
+@NgModule({
+  declarations: [],
+  imports: [DataTableModule],
+  exports: [DataTableModule]
+})
+export class SharedModule {}
